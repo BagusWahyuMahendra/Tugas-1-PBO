0b69468baaa373d0c5cb5a5379faab927169a3fe
baguswahyumahendra <theybagus71@gmail.com>
2023-04-17 22:46:08 +0800

    Membuat Class Menu FIX

--- src/Menu.java
+++ src/Menu.java
@@ -2,15 +2,18 @@ public class Menu {
     private String name;
     private int price;
 
+    //Constructor menu yang memiliki parameter nama dan harga
     public Menu(String name, int price) {
         this.name = name;
         this.price = price;
     }
 
+    //Method getter untuk mendapatkan nilai/value nama
     public String getName() {
         return this.name;
     }
 
+    //Method getter untuk mendapatkan nilai/value price
     public int getPrice() {
         return this.price;
     }
